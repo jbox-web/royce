5d1bce4d9233a34b1aac0efe55b3d5e05494fd5e
Nicolas Rodriguez <nicoladmin@free.fr>
2019-02-18 07:37:42 +0100

    Use Zeitwerk to load gem files

--- lib/royce.rb
+++ lib/royce.rb
@@ -2,14 +2,10 @@
 
 require 'active_record'
 
-module Royce
-  autoload :Macros,       'royce/macros'
-  autoload :Methods,      'royce/methods'
-  autoload :ClassMethods, 'royce/class_methods'
-  autoload :Role,         'royce/role'
-  autoload :Connector,    'royce/connector'
-  autoload :Schema,       'royce/schema'
+require 'zeitwerk'
+Zeitwerk::Loader.for_gem.setup
 
+module Royce
   # Every ::ActiveRecord::Base now includes Royce::Macros
   # This gives them access to the royce_roles method
   ::ActiveRecord::Base.send(:include, Royce::Macros)
