90971cdb3e8ca97c988b6b5cd52b7f9288ce3d29
Nicolas Rodriguez <nicoladmin@free.fr>
2019-02-19 02:19:16 +0100

    Improve DB schema

--- lib/generators/royce/templates/create_royce.rb
+++ lib/generators/royce/templates/create_royce.rb
@@ -1,22 +1,19 @@
 class CreateRoyce < ActiveRecord::Migration
-
   def change
-
     create_table :royce_connector do |t|
       t.references :roleable, polymorphic: true, null: false
       t.references :role, null: false
       t.timestamps
-    end
 
-    add_index :royce_connector, [:roleable_id, :roleable_type]
-    add_index :royce_connector, :role_id
+      t.index :role_id
+      t.index [:roleable_id, :roleable_type]
+    end
 
     create_table :royce_role do |t|
       t.string :name, null: false
       t.timestamps
+
+      t.index :name
     end
-
-    add_index :royce_role, :name
-
   end
 end
--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -7,18 +7,19 @@ ActiveRecord::Schema.define do
     t.integer  :role_id,       null: false
     t.datetime :created_at
     t.datetime :updated_at
+
+    t.index [:role_id]
+    t.index [:roleable_id, :roleable_type]
   end
 
-  add_index :royce_connector, [:role_id], name: :index_royce_connector_on_role_id
-  add_index :royce_connector, [:roleable_id, :roleable_type], name: :index_royce_connector_on_roleable_id_and_roleable_type
 
   create_table :royce_role, force: true do |t|
     t.string   :name,       null: false
     t.datetime :created_at
     t.datetime :updated_at
-  end
 
-  add_index :royce_role, [:name], name: :index_royce_role_on_name
+    t.index :name
+  end
 
   create_table :users, force: true do |t|
     t.datetime :created_at
