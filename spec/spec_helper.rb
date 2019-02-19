efb8fd666972ef9ec798973117fbaa2a2b30aa32
Nicolas Rodriguez <nicoladmin@free.fr>
2019-02-19 04:38:58 +0100

    Improve RSpec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,27 +1,16 @@
 require 'simplecov'
-require 'rspec'
 
 # Start SimpleCov
 SimpleCov.start do
   add_filter 'spec/'
 end
 
-# Configure RSpec
-RSpec.configure do |config|
-  config.color = true
-  config.fail_fast = false
-
-  config.order = :random
-  Kernel.srand config.seed
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
-end
-
 # Load Rails dummy app
 ENV['RAILS_ENV'] = 'test'
 require File.expand_path('dummy/config/environment.rb', __dir__)
 
-# Set DB schema
-ActiveRecord::Migration.maintain_test_schema!
+# Load test gems
+require 'rspec/rails'
+
+# Load our own config
+require_relative 'config_rspec'
