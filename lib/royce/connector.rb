6e02978948dd5787954a8bdeec32e986e0de610d
Nicolas Rodriguez <nicoladmin@free.fr>
2019-02-19 02:05:16 +0100

    Coding style

--- lib/royce/connector.rb
+++ lib/royce/connector.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 module Royce
-  # Connects many roleables and roles
   class Connector < ::ActiveRecord::Base
+
+    # Table name
     self.table_name = 'royce_connector'
+
+    # Relations
     belongs_to :roleable, polymorphic: true
     belongs_to :role, class_name: 'Royce::Role'
   end
--- lib/royce/role.rb
+++ lib/royce/role.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 module Royce
-
-  # The actual data model
   class Role < ::ActiveRecord::Base
+
+    # Table name
     self.table_name = 'royce_role'
 
+    # Relations
     has_many :connectors, class_name: 'Royce::Connector'
 
     def to_s
       name
     end
   end
-
 end
